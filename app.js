c8aabfc4dbf2efa6114fdaf72ec560355ac168be
𝐅𝐀𝐑𝐌𝐀𝐍 𝐀𝐅𝐑𝐈𝐃𝐈 <farman.afridi.257005@gmail.com>
2025-08-12 13:38:24 -0700

    Add files via upload
    Presentation in js Array , for loop

---
+++ app.js
@@ -0,0 +1,33 @@
+// Presentation
+
+// Array 
+
+// var skills = ["html","css","js","react"];
+
+// skills.push("next js") // last element ko add karega
+//skills.pop() // last element ko remove karega
+// skills.shift() // first element remove karega
+// skills.unshift() // ye dobara first element add karega
+// skills.includes() // true ,false deta hai
+// skills.splice(1,3 "next js")
+// skills.slice(1,.2) ye hume shelow copy deta hai
+// console.log(skills.includes("html"))
+
+
+// Foor loop
+
+// var skills = ("html","css","js","react");
+
+// for(var i = 0; i < 5 ; i++){
+//     console.log(i)
+//     break
+    
+// }
+
+
+
+
+//(var i = 0; pehle initialization chale ga
+// i < 5 ; per conditon chale ghi
+//i++ per updation chale gha 
+
