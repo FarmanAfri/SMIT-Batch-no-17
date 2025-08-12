af88c00d75c324eed841ea419d4227ab0ee67b88
𝐅𝐀𝐑𝐌𝐀𝐍 𝐀𝐅𝐑𝐈𝐃𝐈 <farman.afridi.257005@gmail.com>
2025-08-12 13:48:39 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,167 @@
-// Presentation
+// var userInput = prompt("where are you saylani");
 
-// Array 
+// if(userInput != "Multan"){
+//     console.log("correct answer")
+// }else{
+//     console.log("wrong answer")
+// }
 
-// var skills = ["html","css","js","react"];
+// var userName = prompt("Enter Your Name");
+// if(userName){
+//     console.log(userName)
+// }else{
+//     console.log("not provide")
+// }
 
-// skills.push("next js") // last element ko add karega
-//skills.pop() // last element ko remove karega
-// skills.shift() // first element remove karega
-// skills.unshift() // ye dobara first element add karega
-// skills.includes() // true ,false deta hai
-// skills.splice(1,3 "next js")
-// skills.slice(1,.2) ye hume shelow copy deta hai
-// console.log(skills.includes("html"))
+// var userName = prompt("Enter Your Name");
+// var isAvailable = userName && "Yes";
+
+// console.log(isAvailable);
+// console.log(userName);
+
+// six value false in javascript or interview question
+// ""
+// false
+// 0
+// undefined
+// null
+// NaN
+
+// var userName = prompt("Enter your Name");
+// if(userName === "Ali"){
+//     console.log("correct")
+// }else if(userName !== "Farman"){
+//      console.log("InCorrect")
+// }else{
+//     console.log("is name not allowed")
+// }
+
+// var skills = prompt("Enter your skills");
+// if(skills === "html"){
+//     console.log("10000")
+// }else if(skills === "css"){
+//     console.log("15000")
+// }else if(skills === "js"){
+//     console.log("25000")
+// }else {
+//     console.log("not apply")
+// }
+
+// var skills = prompt("Enter Your Skill")
+// var exp = prompt("Enter Your Exp");
+
+// if(skills === "js" && exp > 3){
+//     console.log("Hired")
+// }else{
+//    console.log("not hired") 
+// }
 
 
-// Foor loop
+// var skills = prompt("Enter your skills");
+// var exp = prompt("Enter your exp");
+// var city  = prompt("Enter your city");
+// var graduation = prompt("Enter YOur Graduation");
 
-// var skills = ("html","css","js","react");
-
-// for(var i = 0; i < 5 ; i++){
-//     console.log(i)
-//     break
-    
+// if((skills === "javascript" || exp > 3) && city === "karachi" && graduation === "computer science"){
+//     console.log("Hired")
+// }else{
+//     console.log("Not Hired")
 // }
 
 
 
 
-//(var i = 0; pehle initialization chale ga
-// i < 5 ; per conditon chale ghi
-//i++ per updation chale gha 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
