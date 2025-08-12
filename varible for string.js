a70f09608d7d9d84228df298ea737f84a35cddaa
𝐅𝐀𝐑𝐌𝐀𝐍 𝐀𝐅𝐑𝐈𝐃𝐈 <farman.afridi.257005@gmail.com>
2025-08-12 13:44:55 -0700

    Add files via upload

---
+++ varible for string.js
@@ -0,0 +1,83 @@
+// Alert
+// alert("hello world's")
+
+// Varible For string
+
+//var firstName = "Farooq"; // alert me qutation nahi lagega our console me laga ga
+
+//var lastName = "Afridi";
+
+//alert(firstName + lastName)
+ //console.log(firstName+" "+lastName)  //esme bhi qutation nahi lagega esliye ke var me qutation hai
+
+//var -name
+//var _name
+//var $name
+
+//var myNameLast // camel case or dromedary
+//mynamelastname // flate case
+//MY-NAME-LAST //Train case
+//my-Name-Last snake Case
+// pascle case --\MyNameLast
+
+// var totalMark = 1200;
+// var percen = 100;
+// var contain = 700;
+
+// var result = contain / totalMark * percen;
+
+// // console.log(result)
+
+// // Variable Name Legle and Illegle
+
+// chap no 4
+
+//var name = "name1";  //redeclare
+
+//name = "name3" // resssign
+
+//console.log("name")
+
+//const names = "farman" // reassign const ki value fixed hoti hai ye reassign kabhi nahi hoga
+//names = "khan"
+//console.log(names)
+
+// chap no 5
+
+//  var num = 4;
+//  var num2 = 2;
+
+//  var result = num + num2;
+
+//  console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
